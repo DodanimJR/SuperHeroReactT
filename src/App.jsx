8523cf50fa77b6651d69e24d48a746221d049255
DodanimJr <dodanimjunior24@gmail.com>
2022-07-10 11:17:49 -0600

    Button Random Hero Works!

--- src/App.jsx
+++ src/App.jsx
@@ -8,86 +8,53 @@ import Switch from '@mui/material/Switch';
 
 import './App.css';
 
+
+
 const App = () => {
   const [data, setData] = useState(null);
-  const [loaded, setLoaded] = useState(false);
-  // const fetchData = async (Id) => {
-  //   try{
-  //     const result = await axios.get(`https://cors-anywhere.herokuapp.com/https://www.superheroapi.com/api/2223169807851173/${Id}`);
-  //     console.log("result data:",result.data);
-  //     if (result){
-  //       return result.data
-  //     }
-  //     //console.log(result.data);
-  //     //return result.data.response
-  //   } catch (err){
-  //     console.error(err);
-  //   }
-  // }
-  // const getData = async () => {
-  //   const result = await fetchData(50);
-  //   try {
-  //     if (result){
-  //       setData({result});
-        
-  //       if(data!=null){
-  //         console.log("data:",data);
-  //       }
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-  
-  // if(!data){
-  //   getData();
-  // }
-
-  // if(data!=null){
-  //   console.log("data",data);
-  //   console.log("data.result",data.result);
-  // }
-
+  const [SearchNewHero, setSearchNewHero] = useState(false);
+  const [SearchFavorite, setSearchFavorite] = useState(false);
+  function generateRandomInteger(max) {
+    return Math.floor(Math.random() * max) + 1;
+  }
   useEffect(() => {
     const fetchData = async (url, hook) => {
       try{
         const result = await axios.get(url);
-
+        if(SearchNewHero!=false){setSearchNewHero(!SearchNewHero);}
         hook(result.data);
       } catch (err){
         console.error(err);
       }
     }
-    if(!loaded){
+    if(!data){
       //`https://cors-anywhere.herokuapp.com/https://www.superheroapi.com/api/2223169807851173/${Id}`
-      fetchData("https://cors-anywhere.herokuapp.com/https://www.superheroapi.com/api/2223169807851173/50", setData);
+      fetchData("https://cors-anywhere.herokuapp.com/https://www.superheroapi.com/api/2223169807851173/1", setData);
     }
-  }, [loaded]);
+    if(SearchNewHero!=false){
+      const randomId=generateRandomInteger(700);
+      fetchData(`https://cors-anywhere.herokuapp.com/https://www.superheroapi.com/api/2223169807851173/${randomId}`, setData);
+    }
+  }, [SearchNewHero]);
   const buttonClick = () => {
-    setLoaded(!loaded);
+    setSearchNewHero(!SearchNewHero);
   }
   console.log("data: ",data);
   return (
     
-    <div className="MainContainer">
+    <div className="MainDiv">
       <div className="loaderContainer">
-      <h2>{data === null && <CircularProgress size={60} />}</h2>
+      <h2>{data === null && <CircularProgress size={600} />}</h2>
       </div>
-      <div className="buttonCargar" >
-        <p>HOLA ANDRE</p>
-        <button onClick={buttonClick}>Cargar</button>
+      <div className="listaHeroes" >
+        <button onClick={buttonClick}>RANDOM HERO</button>
       </div>
       <div className="container">
         <h2>HOLA MUNDO</h2>
-        
+        <div className="fullInfo">
+        {data!==null && <SimpleView data={data} />}
+        </div>
         {/* <p>{}</p> */}
-      </div>
-      <div>
-        {data!==null && <div><p>{data.name}</p></div>}
-        
-      </div>
-      <div>
-        render={data!==null && <SimpleView data={data} />}
       </div>
     </div>
     
--- src/mainView.jsx
+++ src/mainView.jsx
@@ -1,20 +1,56 @@
 import React from 'react';
 
-const SimpleView = ({ apidata }) => {
-    console.log("apidata:",apidata);
+const SimpleView = (apidata) => {
+    //console.log("apidata:",apidata.data);
+    try {
+        const finaldata = apidata.data;
+
+        const response = <div className='superHeroFullInfo'>
+            <div className='superHeroMainInfo'>
+                <h1>{finaldata.name}</h1>
+                <img src={finaldata.image.url} alt="" />
+                <h2>{finaldata.slug}</h2>
+                <div className='superHeroWorks'>
+                    <h3>{finaldata.work.base}</h3>
+                    <h3>{finaldata.work.occupation}</h3>
+                    <h3>Connections:</h3>
+                    <h4>Relatives:{finaldata.connections.relatives!==null &&finaldata.connections.relatives}</h4>
+                    <h4>{finaldata.connections["group-affiliation"]!==null &&finaldata.connections["group-affiliation"]}</h4>
+                </div>
+            </div>
+            <div className='powerStats'>
+                <h3>Intelligence:{finaldata.powerstats.intelligence!==null && finaldata.powerstats.intelligence}</h3>
+                <h3>Strength:{finaldata.powerstats.strength!==null &&finaldata.powerstats.strength}</h3>
+                <h3>Speed:{finaldata.powerstats.speed!==null &&finaldata.powerstats.speed}</h3>
+                <h3>Durability:{finaldata.powerstats.durability!==null &&finaldata.powerstats.durability}</h3>
+                <h3>Power:{finaldata.powerstats.power!==null &&finaldata.powerstats.power}</h3>
+                <h3>Combat:{finaldata.powerstats.combat!==null &&finaldata.powerstats.combat}</h3>
+            </div>
+            <div className='appearance'>
+                <p>{finaldata.appearance.gender}</p>
+                <p>{finaldata.appearance.race}</p>
+                <p>{finaldata.appearance["eye-color"]}</p>
+                <p>{finaldata.appearance["hair-color"]}</p>
+                <p>{finaldata.appearance.height}</p>
+                <p>{finaldata.appearance.weight}</p>
+            </div>
+            <div className='biography'>
+                <p>Full name:{finaldata.biography["full-name"]}</p>
+                <p>Alter egos:{finaldata.biography["alter-egos"]}</p>
+                <p>Aliases:{finaldata.biography.aliases}</p>
+                <p>Place of birth:{finaldata.biography["place-of-birth"]}</p>
+                <p>First appearance:{finaldata.biography["first-appearance"]}</p>
+                <p>Publisher: {finaldata.biography.publisher}</p>
+                <p>Alignment:{finaldata.biography.alignment}</p>
+            </div>
+        </div>
+        return (response);
+    } catch (error) {
+        return(<div>
+            <h1>{error}</h1>
+        </div>);
+        
+    }
 }
 
 export default SimpleView;
-// {
-//     "data": {
-//       "response": "success",
-//       "id": "50",
-//       "name": "Atom",
-//       "powerstats": "{combat: \"null\", durability: \"null\", intelligence: …}",
-//       "biography": "{aliases: Array(2), alignment: \"good\", alter-egos: …}",
-//       "appearance": "{eye-color: \"Blue\", gender: \"Male\", hair-color: \"Re…}",
-//       "work": "{base: \"GBS Building, Gotham City (former) Calvin C…}",
-//       "connections": "{group-affiliation: \"formerly Justice Society of Am…}",
-//       "image": "{url: \"https://www.superherodb.com/pictures2/portra…}"
-//     }
-//   }
